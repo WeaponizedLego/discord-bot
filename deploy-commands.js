ee166418dc1605d2b4a8f4f2c1cd737b2dd35217
Glen Jensen <glen@soop.dk>
2023-04-06 22:04:12 +0200

    gitflow-feature-stash: switchtoTS

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,18 +1,19 @@
+const { exec } = require('child_process')
 const { REST, Routes } = require('discord.js')
-const { clientId, guildId, token } = require('./config.json')
+const { clientId, guildId, token } = require('./dist/config.json')
 const fs = require('node:fs')
 const path = require('node:path')
 
 const commands = []
 // Grab all the command files from the commands directory you created earlier
-const commandsPath = path.join(__dirname, 'commands')
+const commandsPath = path.join(__dirname, 'dist/commands')
 const commandFiles = fs
   .readdirSync(commandsPath)
   .filter((file) => file.endsWith('.js'))
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`)
+  const command = require(`./dist/commands/${file}`)
   commands.push(command.data.toJSON())
 }
 
@@ -20,21 +21,31 @@ for (const file of commandFiles) {
 const rest = new REST({ version: '10' }).setToken(token)
 
 // and deploy your commands!
-;(async () => {
+;(() => {
   try {
-    console.log(
-      `Started refreshing ${commands.length} application (/) commands.`
-    )
+    exec('npm run build', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`)
+        throw error
+      }
+      console.log(`stdout: ${stdout}`)
+      console.error(`stderr: ${stderr}`)
+      ;(async () => {
+        console.log(
+          `Started refreshing ${commands.length} application (/) commands.`
+        )
 
-    // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands }
-    )
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+          Routes.applicationGuildCommands(clientId, guildId),
+          { body: commands }
+        )
 
-    console.log(
-      `Successfully reloaded ${data.length} application (/) commands.`
-    )
+        console.log(
+          `Successfully reloaded ${data.length} application (/) commands.`
+        )
+      })()
+    })
   } catch (error) {
     // And of course, make sure you catch and log any errors!
     console.error(error)
